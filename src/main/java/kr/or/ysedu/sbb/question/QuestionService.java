ef7b1eeda679dfda0102cbfa0601397f5d8658c0
yeong <webmake2050@gmail.com>
2025-08-26 08:45:42 +0900

    상세 페이지에 서비스 사용하기

--- src/main/java/kr/or/ysedu/sbb/question/QuestionService.java
+++ src/main/java/kr/or/ysedu/sbb/question/QuestionService.java
@@ -1,9 +1,11 @@
 package kr.or.ysedu.sbb.question;
 
+import kr.or.ysedu.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,8 +14,16 @@ public class QuestionService {
   private final QuestionRepository questionRepository;
 
   public List<Question> getList() {
-
     return this.questionRepository.findAll();
+  }
+
+  public Question getQuestion(Integer id) {
+    Optional<Question> question = this.questionRepository.findById(id);
+    if (question.isPresent()) {
+      return question.get();
+    } else  {
+      throw new DataNotFoundException("Question not found");
+    }
   }
 
 }
