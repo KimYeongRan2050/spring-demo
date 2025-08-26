718ef0736765bd3b73553ca72fd8acfd95da0f56
yeong <webmake2050@gmail.com>
2025-08-26 15:28:19 +0900

    답변 서비스 만들기

--- src/main/java/kr/or/ysedu/sbb/answer/AnswerController.java
+++ src/main/java/kr/or/ysedu/sbb/answer/AnswerController.java
@@ -1,8 +1,10 @@
 package kr.or.ysedu.sbb.answer;
 
+import kr.or.ysedu.sbb.question.Question;
 import kr.or.ysedu.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AnswerController {
   private final QuestionService questionService;
+  private final AnswerService answerService;
 
   @PostMapping("/create/{id}")
   public String createAnswer(Model model,
                              @PathVariable("id") Integer id,
                              @RequestParam(value = "content") String content) {
     Question question = this.questionService.getQuestion(id);
+
+    this.answerService.create(question, content);
     return String.format("redirect:/question/detail/%s", id);
   }
 }
