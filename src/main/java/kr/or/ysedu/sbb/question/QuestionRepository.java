71d028af5b51286b1c2a8d027de83a09782b09ae
yeong <webmake2050@gmail.com>
2025-08-26 17:09:22 +0900

    페이징 구현하기

--- src/main/java/kr/or/ysedu/sbb/question/QuestionRepository.java
+++ src/main/java/kr/or/ysedu/sbb/question/QuestionRepository.java
@@ -1,5 +1,7 @@
 package kr.or.ysedu.sbb.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,6 +10,7 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
   Question findBySubject(String subject);
   Question findBySubjectAndContent(String subject, String content);
   List<Question> findBySubjectLike(String subject);
+  Page<Question> findAll(Pageable pageable);
 }
 /*
 == 리포지터리 생성하기 ==
